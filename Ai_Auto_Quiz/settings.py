6723d46b2e899a65d6af77d893b17032e33a561d
Indrive246 <ebediakoh7@gmail.com>
2025-07-26 17:05:09 +0000

    Update settings.py

--- Ai_Auto_Quiz/settings.py
+++ Ai_Auto_Quiz/settings.py
@@ -10,10 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y^s02#q+-4znk6vnp90c(e&y5((^q+h%fc&l)mmc1&z4f1zh6v'
+OPENAI_API_KEY = env('OPENAI_API_KEY')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
